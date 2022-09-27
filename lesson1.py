278d2af6611106c7549764aac58bd34dd026243c
geto133 <getia.giorgi00@gmail.com>
2022-09-27 16:30:49 +0400

    :

---
+++ lesson1.py
@@ -0,0 +1,35 @@
+name = "me var Giorgi" #ეს არის str (string) ტიპის ცვლადი
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "giorgi" არის ცვლადისთვისს მნიშვნელობა
+surname = "getia"
+
+#PRINT(NAME)
+#პრინტ ფუქნციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+education = "Vswavlob ilia state university"
+faculty = "var 3 kursis studenti samartalze. asaki:"
+age = 21   #ეს არის int(integer) მთელი რიცხვი 
+height = 186.5
+#ეს არის float ტიპის ცვლადი (ათწილადი)
+
+
+#boolean (bool) ტიპის ცვლადები
+knows_programming = True #True ან False snakecase (წერის სტილი)
+isUgly = False # ჯავასკრიპტული cameLcase
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + " " + surname)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))    
+#print(type(height))
+#print(type(knows_programming))
+#ასე შეგვიძლია გავიგო რომელი სტილი არის str int float ან სხვა
+
+#print(name+ " " + str(age)) #ასე შეგვიძლია შევუცვლოთ სტილი
+
+print(name + " " + surname +" "+  education +" "+ faculty +" " + str(age) + " " + str(height) )
+
+
